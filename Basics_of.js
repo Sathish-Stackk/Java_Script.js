28469ab02c7b07bad73b6acf902cc03e158f8834
Mungi Sathish <sathishmungi@gmail.com>
2026-08-15 23:56:47 +0530

    Create Basics_of.js

---
+++ Basics_of.js
@@ -0,0 +1,16 @@
+let name = "JavaScript";
+
+console.log(name);
+
+// Time Complexity: O(1)
+
+let text = "College Coders";
+
+console.log(text.length);
+// Time Complexity: O(1)
+
+let text = "Hello";
+
+for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+// Time Complexity: O(n)
