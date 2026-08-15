8b3108da1bf5381df9d7219432f5e7414d5aadfa
Mungi Sathish <sathishmungi@gmail.com>
2026-08-15 23:58:13 +0530

    Create Concatenate_Two_Strings.js

---
+++ Concatenate_Two_Strings.js
@@ -0,0 +1,17 @@
+let first = "Hello";
+let second = "World";
+
+let result = first + " " + second;
+
+console.log(result);
+
+// Time Complexity: O(n)
+. 
+
+// Convert a String to Uppercase
+let text = "javascript";
+
+
+console.log(text.toUpperCase());
+
+// Time Complexity: O(n)
