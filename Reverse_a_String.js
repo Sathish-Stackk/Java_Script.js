ff28d9291d084966254efb56e5d9fd34f3ed5cd4
Mungi Sathish <sathishmungi@gmail.com>
2026-08-17 21:11:10 +0530

    Create Reverse_a_String.js

---
+++ Reverse_a_String.js
@@ -0,0 +1,14 @@
+let str = "JavaScript";
+let reversed = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+}
+
+console.log(reversed);
+
+
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(n)
