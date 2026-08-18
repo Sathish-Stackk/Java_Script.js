c99d63712a1cfa1989ac93a1b9918502a2bf2ec5
Mungi Sathish <sathishmungi@gmail.com>
2026-08-18 21:47:31 +0530

    Create Check_leap_year.js

---
+++ Check_leap_year.js
@@ -0,0 +1,12 @@
+let year = 2028;
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log("Leap Year");
+} else {
+    console.log("Not a Leap Year");
+}
+
+
+// Time Complexity: O(1)
+
+// Space Complexity: O(1)
