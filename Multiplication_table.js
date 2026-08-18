4df70812e488ff99ad9ed136ae42e7ba0e811fe6
Mungi Sathish <sathishmungi@gmail.com>
2026-08-18 21:48:49 +0530

    Create Multiplication_table.js

---
+++ Multiplication_table.js
@@ -0,0 +1,9 @@
+let number = 7;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " x " + i + " = " + number * i);
+}
+
+// Time Complexity: O(1) (10 iterations)
+
+// Space Complexity: O(1)
