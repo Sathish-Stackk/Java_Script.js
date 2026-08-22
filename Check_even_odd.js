3bdc8c2960098919fadf45bdc2f8a00bdf7e9381
Mungi Sathish <sathishmungi@gmail.com>
2026-08-22 15:46:35 +0530

    Create Check_even_odd.js

---
+++ Check_even_odd.js
@@ -0,0 +1,19 @@
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+checkEvenOdd(7);
+
+// Check Even or Odd//
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(6));
+
+// . Find Square of a Number
