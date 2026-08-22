05ebc0f4942d3ecc6506c181d0ad5d1e3a8b860c
Mungi Sathish <sathishmungi@gmail.com>
2026-08-22 15:42:49 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,32 @@
+//simple function//
+function greet() {
+    console.log("Hello, World!");
+}
+
+greet();
+
+function greetUser(name) {
+    console.log("Hello " + name);
+}
+
+greetUser("Sathish");
+
+//Function with a Parameter//
+
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(10, 20);
+
+//add two numbers
+
+function subtract(a, b) {
+    console.log(a - b);
+}
+
+subtract(50, 20);
+
+//substract two nums
+
