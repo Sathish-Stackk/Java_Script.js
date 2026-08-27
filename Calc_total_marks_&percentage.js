5e6b475b23d4e322e16c0c65cc1aac9b3cf17e30
Mungi Sathish <sathishmungi@gmail.com>
2026-08-27 22:44:52 +0530

    Create Calc_total_marks_&percentage.js

---
+++ Calc_total_marks_&percentage.js
@@ -0,0 +1,9 @@
+let math = 80;
+let science = 75;
+let english = 85;
+
+let total = math + science + english;
+let percentage = total / 3;
+
+console.log("Total Marks:", total);
+console.log("Percentage:", percentage + "%");
